5b5afdc27c57177c850d31d3e3079be032843029
dmoosey <56768881+dmoosey@users.noreply.github.com>
2021-02-10 21:42:33 +0000

    clean up comments

--- utils/scrape.js
+++ utils/scrape.js
@@ -1,13 +1,13 @@
 const champion_data = require('../data/champs.json');
 
 champ = (champ_input) => {
+    // Initialize an array identifying which champs data is available for
     valid_champ_names = Object.keys(champion_data);
 
     // Returns basic information from champs.json for specified champion
+    // champ_name should be in 'key' format e.g TwistedFate
     const get_JSON_data = (champ_name) => {
-
-        //if (!valid_champ_names.includes(champ_name)) throw new TypeError(`Error: Invalid champion name: ('${champ_name}')`)
-
+        // Only valid champs will make it this far so we can just write grab and return the data we need
         const champ_obj = champion_data[champ_name]
         return {
             id: champ_obj["id"],
@@ -18,35 +18,39 @@ champ = (champ_input) => {
         }
     }
 
-
     // Identify JSON inputs
     if (typeof champ_input == 'object' && !Array.isArray(champ_input)) {
         champ_input = Object.keys(champ_input)
     };
 
-    // Identify Single champ strings
+    // Identify queries for a single champ
     if (typeof champ_input == 'string') {
         champ_input = new Array(champ_input)
     };
 
-    // return Filtered Array with only valid names
+    // return Filtered Array with only valid names (ones that we have data for)
     champ_input_filtered = champ_input.filter((v, i) => {
         return valid_champ_names.includes(v)
     });
 
+    // How many champs are we working with
     champ_count = champ_input_filtered.length;
-
+    
+    // All champs were invalid and have been filtered out
     if(champ_count == 0) throw new TypeError(`Error: No valid champion names provided`)
 
+    // For single queries no container object is needed so just return the single object
     if(champ_count == 1) return get_JSON_data(champ_input_filtered[0]) 
 
+    // Container object
     champs = {};
 
+    // Iterate over our filtered array retreiving and writing our champ objects into their container
     champ_input_filtered.forEach(champ_name => {
         champ_data = get_JSON_data(champ_name);
         champs[champ_name] = champ_data;
     })
-
+    // return master object
     return champs
 }
 
