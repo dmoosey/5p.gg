f687d05199a4da9c5ba7064174e09317cb70a0c7
dmoosey <56768881+dmoosey@users.noreply.github.com>
2021-02-12 19:42:38 +0000

    refactor test code

--- test/webscraper-test.js
+++ test/webscraper-test.js
@@ -2,7 +2,7 @@ var assert = require('assert');
 var Scrape = require('../utils/scrape');
 
 describe('Scrape', () => {
-    describe('#champ', () => {
+    describe('#JSON_data', () => {
         it('returns an object containing local (champs.json) data for the given champion', () => {
             const input = "Fizz"
             const expected = {
@@ -12,8 +12,35 @@ describe('Scrape', () => {
                 title : "the Tidal Trickster",
                 tags : ["Assassin","Fighter"]
             }
-            const result = Scrape.champ(input);
+            const result = Scrape.JSON_data(input);
             assert.deepStrictEqual(result, expected);
+        })
+        it('raises a type error when an invalid champion input is passed', () => {
+            const input = 'Jhizz';
+            const result = () => {Scrape.JSON_data(input)};
+            assert.throws(result, /Invalid Champion name/);
+        })
+    })
+    describe('#OPGG_data', () => {
+        describe('#overview', () => {
+            it('returns an object containing data from champs op.gg overview page', () => {
+                const input = "Fizz";
+                const expected = {
+                    roles : ["Middle"],
+                    tier : "Tier 2" 
+                }
+                const result = Scrape.OPGG_data.overview(input)
+                assert.deepStrictEqual(result, expected);
+            })
+        })
+    })
+    describe('#champ', () => {
+        it('returns a champion object containing data for the provided champion', () => {
+            const input = "Fizz";
+            const expected = true;
+            const result = Scrape.champ(input);
+            const compare = typeof result && (Object.keys(result).length > 0);
+            assert.strictEqual(compare, expected);
         })
         it('returns multiple nested objects when an array of champions is given', () => {
             const input = ["Fizz", "Zoe"];
--- utils/scrape.js
+++ utils/scrape.js
@@ -1,13 +1,14 @@
+const rp = require('request-promise');
+const $ = require('cheerio');
+const fs = require('fs');
+
 const champion_data = require('../data/champs.json');
 
-champ = (champ_input) => {
-    // Initialize an array identifying which champs data is available for
-    valid_champ_names = Object.keys(champion_data);
-
-    // Returns basic information from champs.json for specified champion
-    // champ_name should be in 'key' format e.g TwistedFate
-    const get_JSON_data = (champ_name) => {
-        // Only valid champs will make it this far so we can just write grab and return the data we need
+// Returns basic information from champs.json for specified champion
+// champ_name should be in 'key' format e.g TwistedFate
+JSON_data = (champ_name) => {
+    // Only valid champs will make it this far so we can just write grab and return the data we need
+    try{
         const champ_obj = champion_data[champ_name]
         return {
             id: champ_obj["id"],
@@ -17,6 +18,28 @@ champ = (champ_input) => {
             tags: champ_obj["tags"]
         }
     }
+    catch(err){
+        throw new TypeError(`Invalid Champion name provided`)
+    }
+}
+
+OPGG_data = {
+    // Returns general information from op.gg for specified champion#
+    // champ_name should be in 'key' format e.g TwistedFate
+    overview : async (champ_name) => {
+        // fetch champ overview page
+        const url = `https://euw.op.gg/champion/${champ_input}/statistics/`;
+        const html = await rp(url);
+        
+        // specify class attr value for role element
+        const role_class = 'champion-stats-header__position__role';
+
+    }
+}
+
+champ = (champ_input) => {
+    // Initialize an array identifying which champs data is available for
+    valid_champ_names = Object.keys(champion_data);
 
     // Identify JSON inputs
     if (typeof champ_input == 'object' && !Array.isArray(champ_input)) {
@@ -35,23 +58,23 @@ champ = (champ_input) => {
 
     // How many champs are we working with
     champ_count = champ_input_filtered.length;
-    
+
     // All champs were invalid and have been filtered out
-    if(champ_count == 0) throw new TypeError(`Error: No valid champion names provided`)
+    if (champ_count == 0) throw new TypeError(`No valid champion names provided`)
 
     // For single queries no container object is needed so just return the single object
-    if(champ_count == 1) return get_JSON_data(champ_input_filtered[0]) 
+    if (champ_count == 1) return JSON_data(champ_input_filtered[0])
 
     // Container object
     champs = {};
 
     // Iterate over our filtered array retreiving and writing our champ objects into their container
     champ_input_filtered.forEach(champ_name => {
-        champ_data = get_JSON_data(champ_name);
+        champ_data = JSON_data(champ_name);
         champs[champ_name] = champ_data;
     })
     // return master object
     return champs
 }
 
-module.exports = { champ }
+module.exports = { champ , JSON_data , OPGG_data }
