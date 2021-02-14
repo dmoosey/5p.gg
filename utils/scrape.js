b70cddc3c36e6546f42728b172ebe3cc0f3a38f7
dmoosey <56768881+dmoosey@users.noreply.github.com>
2021-02-14 18:29:17 +0000

    start OPGG_data.items() implementation

--- utils/scrape.js
+++ utils/scrape.js
@@ -21,8 +21,8 @@ validate_input = (champ_input) => {
 // Returns basic information from champs.json for specified champion
 // champ_name should be in 'key' format e.g TwistedFate
 JSON_data = (champ_name) => {
-    if (!this.validate_input(champ_name)) throw new Error("Invalid champion name provided.")
-    
+    if (!validate_input(champ_name)) throw new Error("Invalid champion name provided.")
+
     const champ_obj = champion_data[champ_name]
     return {
         id: champ_obj["id"],
@@ -34,10 +34,12 @@ JSON_data = (champ_name) => {
 }
 
 OPGG_data = {
+    roles_web: { "Middle": "mid", "Bottom": "bot", "Support": "support", "Jungle": "jungle", "Bottom": "bot" },
     // Returns general information from op.gg for specified champion#
-    // champ_name should be in 'key' format e.g TwistedFate
-    overview: async (champ_name) => {
-        if (!this.validate_input(champ_name)) throw new Error("Invalid champion name provided.")
+    // champ_input is the object returned from JSON_data(champ_name)
+    overview: async (champ_input) => {
+        const champ_name = champ_input.name;
+        if (!validate_input(champ_name)) throw new Error("Invalid champion name provided.")
         // fetch champ overview page
         const url = `https://euw.op.gg/champion/${champ_name}/statistics/`;
         const html = await rp(url);
@@ -62,50 +64,67 @@ OPGG_data = {
             const b_tag = div_class[0].children[1];
             tier = b_tag.children[0].data;
         }
+        let overview = { roles, tier };
+        let champ_general = Object.assign(champ_input, overview);
 
-        return {
-            roles,
-            tier,
+        return champ_general
+
+    },
+    // Returns information on item builds for the specified champ
+    // champ_input is the object returned from OPGG_data()
+    items: async (champ_input) => {
+        const champ_name = champ_input.name;
+        for (const role of champ_input.roles) {
+            const role_key = OPGG_data.roles_web[role];
+            const url = `https://euw.op.gg/champion/${champ_name}/statistics/${role_key}/item`;
+            const html = await rp(url);
+            let core = [];
+            let boots = '';
+            let starter = [];
+
+            const core_class = '.champion-stats__list';
+
+            const ul_tag = $(core_class, html);
+            console.log(ul_tag);
         }
-
     }
 }
 
 champ = (champ_input) => {
-    // Identify JSON inputs
-    if (typeof champ_input == 'object' && !Array.isArray(champ_input)) {
-        champ_input = Object.keys(champ_input)
-    };
+        // Identify JSON inputs
+        if (typeof champ_input == 'object' && !Array.isArray(champ_input)) {
+            champ_input = Object.keys(champ_input)
+        };
 
-    // Identify queries for a single champ
-    if (typeof champ_input == 'string') {
-        champ_input = new Array(champ_input)
-    };
+        // Identify queries for a single champ
+        if (typeof champ_input == 'string') {
+            champ_input = new Array(champ_input)
+        };
 
-    // return Filtered Array with only valid names (ones that we have data for)
-    champ_input_filtered = champ_input.filter((v, i) => {
-        return validate_input(v)
-    });
+        // return Filtered Array with only valid names (ones that we have data for)
+        champ_input_filtered = champ_input.filter((v, i) => {
+            return validate_input(v)
+        });
 
-    // How many champs are we working with
-    champ_count = champ_input_filtered.length;
+        // How many champs are we working with
+        champ_count = champ_input_filtered.length;
 
-    // All champs were invalid and have been filtered out
-    if (champ_count == 0) throw new TypeError(`No valid champion names provided`)
+        // All champs were invalid and have been filtered out
+        if (champ_count == 0) throw new TypeError(`No valid champion names provided`)
 
-    // For single queries no container object is needed so just return the single object
-    if (champ_count == 1) return JSON_data(champ_input_filtered[0])
+        // For single queries no container object is needed so just return the single object
+        if (champ_count == 1) return JSON_data(champ_input_filtered[0])
 
-    // Container object
-    champs = {};
+        // Container object
+        champs = {};
 
-    // Iterate over our filtered array retreiving and writing our champ objects into their container
-    champ_input_filtered.forEach(champ_name => {
-        champ_data = JSON_data(champ_name);
-        champs[champ_name] = champ_data;
-    })
-    // return master object
-    return champs
-}
+        // Iterate over our filtered array retreiving and writing our champ objects into their container
+        champ_input_filtered.forEach(champ_name => {
+            champ_data = JSON_data(champ_name);
+            champs[champ_name] = champ_data;
+        })
+        // return master object
+        return champs
+    }
 
 module.exports = { champ, JSON_data, OPGG_data, validate_input }
