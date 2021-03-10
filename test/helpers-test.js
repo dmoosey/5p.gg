c97ec8aed81832953e36daa3d313d54ad1e8df51
dmoosey <56768881+dmoosey@users.noreply.github.com>
2021-03-10 14:29:51 +0000

    test img to str helper

--- test/helpers-test.js
+++ test/helpers-test.js
@@ -3,9 +3,15 @@ const Helpers = require('../utils/helpers');
 
 describe('Helpers', () => {
     describe('imgurl_to_string', () => {
-        it('returns the name of the item in the image', () => {
-            const input = '//opgg-static.akamaized.net/images/lol/item/4636.png?image=q_auto:best&v=1612855207';
-            const expected = 'Night Harvester';
+        it('returns the name of the rune in the image (Keystone)', () => {
+            const input = '//opgg-static.akamaized.net/images/lol/perk/8112.png?image=c_scale,q_auto,w_42&v=1614735388';
+            const expected = 'Electrocute';
+            const result = Helpers.imgurl_to_string(input);
+            assert.equal(result, expected);
+        })
+        it('returns the name of the rune in the image (Tree)', () => {
+            const input = '//opgg-static.akamaized.net/images/lol/perkStyle/8100.png?image=c_scale,q_auto,w_26&v=1614735388';
+            const expected = 'Domination';
             const result = Helpers.imgurl_to_string(input);
             assert.equal(result, expected);
         })
