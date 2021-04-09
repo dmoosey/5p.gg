636f9df8c9d97e992090e813dd692adf1005c6df
dmoosey <56768881+dmoosey@users.noreply.github.com>
2021-04-09 14:31:13 +0100

    start db fetch implementation

---
+++ utils/fetch-db.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const mongo_uri = "mongodb+srv://dmoore:Nkv1Tp0EnrYdChnf@5pgg.xwr1h.mongodb.net/Champion?retryWrites=true&w=majority";
+
+// Connect to the MongoDB cluster
+mongoose.connect(
+    mongo_uri, { useNewUrlParser: true, useUnifiedTopology: true }, () =>
+    console.log(" Mongoose is connected")
+);
+
+const db = mongoose.connection;
