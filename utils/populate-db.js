31898b819d7f2ff33f5424f91a8d11e3f5aed207
dmoosey <56768881+dmoosey@users.noreply.github.com>
2021-04-09 14:21:48 +0100

    fix node mongoose driver error

--- utils/populate-db.js
+++ utils/populate-db.js
@@ -31,16 +31,10 @@ const Champ = mongoose.model('Champ', champ_schema, 'champs');
 const mongo_uri = "mongodb+srv://dmoore:Nkv1Tp0EnrYdChnf@5pgg.xwr1h.mongodb.net/Champion?retryWrites=true&w=majority";
 
 // Connect to the MongoDB cluster
-try {
-    // Connect to the MongoDB cluster
-    mongoose.connect(
-        mongo_uri, { useNewUrlParser: true, useUnifiedTopology: true }, () =>
-        console.log(" Mongoose is connected")
-    );
-
-} catch (e) {
-    console.log("could not connect");
-}
+mongoose.connect(
+    mongo_uri, { useNewUrlParser: true, useUnifiedTopology: true }, () =>
+    console.log(" Mongoose is connected")
+);
 
 const db = mongoose.connection;
 
