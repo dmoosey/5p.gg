51507023257d57f63d3978378e09864c452ffa6d
dmoosey <56768881+dmoosey@users.noreply.github.com>
2021-05-05 22:34:01 +0100

    add more clear example code for later

--- utils/fetch-db.js
+++ utils/fetch-db.js
@@ -1,10 +1,17 @@
 const Champ = require('../data/Champ');
 const db = require('./db');
 
-c = 'Fizz';
-
 db.on('error', console.error.bind(console, 'connection error:'));
 
-db.once('open', async function () {
-    console.log(await Champ.findById(c))
-});
+get_champ_data = async function (c) {
+    champ_data = await Champ.findById(c);
+    return champ_data
+}
+
+log_data = async function (c){
+    champ_data = await get_champ_data(c);
+    console.log(champ_data.overview.title)
+}
+
+log_data('Fizz')
+
