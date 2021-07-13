a3fe601b6472f41493413a47a2164e471414e277
dmoosey <56768881+dmoosey@users.noreply.github.com>
2021-07-13 10:18:49 +0100

    db fetch boilerplate

--- utils/fetch-db.js
+++ utils/fetch-db.js
@@ -10,8 +10,10 @@ get_champ_data = async function (champ) {
 
 fetch_runes = async (parsed_command) => {
     const champ_name = parsed_command.champ.sentence
+
     const champ_data = await get_champ_data(champ_name);
     if (!champ_data) return `Please provide a valid champion name and role`
+    
     const role = parsed_command.lane.raw
     const data = champ_data.runes[role] || `No data found for ${champ_name} in ${role}`;
     return data
